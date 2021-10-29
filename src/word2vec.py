bde00e8865a30a53a93990862a40d3ecaf7ea8fc
いお <wmt.tkn.121@gmail.com>
2021-10-29 16:07:18 +0900

    simpleCBOW学習成功

--- src/word2vec.py
+++ src/word2vec.py
@@ -1,6 +1,10 @@
+# coding: UTF-8
+import numpy as np
+import matplotlib.pyplot as plt
+import time
 import sys
 sys.path.append('..')
-import numpy as np
+
 
 def softmax(x):
     if x.ndim == 2:
@@ -14,12 +18,23 @@ def softmax(x):
     return x
 
 
+def clip_grads(grads, max_norm):
+    total_norm = 0
+    for grad in grads:
+        total_norm += np.sum(grad ** 2)
+    total_norm = np.sqrt(total_norm)
+
+    rate = max_norm / (total_norm + 1e-6)
+    if rate < 1:
+        for grad in grads:
+            grad *= rate
+
+
 def cross_entropy_error(y, t):
     if y.ndim == 1:
         t = t.reshape(1, t.size)
         y = y.reshape(1, y.size)
 
-    # 教師データがone-hot-vectorの場合、正解ラベルのインデックスに変換
     if t.size == y.size:
         t = t.argmax(axis=1)
 
@@ -27,72 +42,103 @@ def cross_entropy_error(y, t):
 
     return -np.sum(np.log(y[np.arange(batch_size), t] + 1e-7)) / batch_size
 
+
 def preprocess(text):
-  text = text.lower()
-  text = text.replace('.',' .')
-  words = text.split(' ')
+    text = text.lower()
+    text = text.replace('.', ' .')
+    words = text.split(' ')
 
-  word_to_id = {}
-  id_to_word = {}
-  for word in words:
-    if word not in word_to_id:
-      new_id=len(word_to_id)
-      word_to_id[word] = new_id
-      id_to_word[new_id] = word
+    word_to_id = {}
+    id_to_word = {}
+    for word in words:
+        if word not in word_to_id:
+            new_id = len(word_to_id)
+            word_to_id[word] = new_id
+            id_to_word[new_id] = word
 
-  corpus = np.array([word_to_id[word] for word in words])
+    corpus = np.array([word_to_id[word] for word in words])
 
-  return corpus,word_to_id,id_to_word
+    return corpus, word_to_id, id_to_word
+
 
 class MatMul:
-  def __init__(self,W):
-    self.params = [W]
-    self.grads = [np.zeros_like(W)]
-    self.x = None
+    def __init__(self, W):
+        self.params = [W]
+        self.grads = [np.zeros_like(W)]
+        self.x = None
 
-  def forward(self,x):
-    W = self.params
-    out = np.dot(x,W)
-    self.x = x
-    return out
+    def forward(self, x):
+        W, = self.params
+        out = np.dot(x, W)
+        self.x = x
+        return out
 
-  def backward(self,dout):
-    W, = self.params
-    dx =np.dot(dout,W.T)
-    dW = np.dot(self.x.T,dout)
-    self.grads[0][...] = dW
-    return dx
+    def backward(self, dout):
+        W, = self.params
+        dx = np.dot(dout, W.T)
+        dW = np.dot(self.x.T, dout)
+        self.grads[0][...] = dW
+        return dx
 
-def create_contexts_target(corpus,window_size=1):
-  target= corpus[window_size:-window_size]
-  contexts=[]
 
-  for idx in range(window_size,len(corpus)-window_size):
-    cs=[]
-    for t in range(-window_size,window_size+1):
-      if(t==0):
-        continue
-      cs.append(corpus[idx+t])
-    contexts.append(cs)
-  return np.array(contexts), np.array(target)
+def create_contexts_target(corpus, window_size=1):
+    '''コンテキストとターゲットの作成
+    :param corpus: コーパス（単語IDのリスト）
+    :param window_size: ウィンドウサイズ（ウィンドウサイズが1のときは、単語の左右1単語がコンテキスト）
+    :return:
+    '''
+    target = corpus[window_size:-window_size]
+    contexts = []
+
+    for idx in range(window_size, len(corpus)-window_size):
+        cs = []
+        for t in range(-window_size, window_size + 1):
+            if t == 0:
+                continue
+            cs.append(corpus[idx + t])
+        contexts.append(cs)
+
+    return np.array(contexts), np.array(target)
+
 
 def convert_one_hot(corpus, vocab_size):
+    '''one-hot表現への変換
+    :param corpus: 単語IDのリスト（1次元もしくは2次元のNumPy配列）
+    :param vocab_size: 語彙数
+    :return: one-hot表現（2次元もしくは3次元のNumPy配列）
+    '''
+    N = corpus.shape[0]
 
-  N = corpus.shape[0]
+    if corpus.ndim == 1:
+        one_hot = np.zeros((N, vocab_size), dtype=np.int32)
+        for idx, word_id in enumerate(corpus):
+            one_hot[idx, word_id] = 1
 
-  if corpus.ndim == 1:
-    one_hot = np.zeros((N, vocab_size), dtype=np.int32)
-    for idx, word_id in enumerate(corpus):
-      one_hot[idx, word_id] = 1
+    elif corpus.ndim == 2:
+        C = corpus.shape[1]
+        one_hot = np.zeros((N, C, vocab_size), dtype=np.int32)
+        for idx_0, word_ids in enumerate(corpus):
+            for idx_1, word_id in enumerate(word_ids):
+                one_hot[idx_0, idx_1, word_id] = 1
 
-  elif corpus.ndim == 2:
-    C = corpus.shape[1]
-    one_hot = np.zeros((N, C, vocab_size), dtype=np.int32)
-    for idx_0, word_ids in enumerate(corpus):
-      for idx_1, word_id in enumerate(word_ids):
-        one_hot[idx_0, idx_1, word_id] = 1
+    return one_hot
 
-  return one_hot
+    N = corpus.shape[0]
+
+    if corpus.ndim == 1:
+        one_hot = np.zeros((N, vocab_size), dtype=np.int32)
+        for idx, word_id in enumerate(corpus):
+            one_hot[idx, word_id] = 1
+
+    elif corpus.ndim == 2:
+        C = corpus.shape[1]
+        one_hot = np.zeros((N, C, vocab_size), dtype=np.int32)
+        for idx_0, word_ids in enumerate(corpus):
+            for idx_1, word_id in enumerate(word_ids):
+                one_hot[idx_0, idx_1, word_id] = 1
+
+    return one_hot
+
 
 class SoftmaxWithLoss:
     def __init__(self):
@@ -122,66 +168,194 @@ class SoftmaxWithLoss:
         return dx
 
 
+class Adam:
+    '''
+    Adam (http://arxiv.org/abs/1412.6980v8)
+    '''
 
-c0=np.array([[1,0,0,0,0,0,0]])
-c1=np.array([[0,0,1,0,0,0,0]])
+    def __init__(self, lr=0.001, beta1=0.9, beta2=0.999):
+        self.lr = lr
+        self.beta1 = beta1
+        self.beta2 = beta2
+        self.iter = 0
+        self.m = None
+        self.v = None
 
-W_in = np.random.randn(7,3)
-W_out = np.random.randn(3,7)
+    def update(self, params, grads):
+        if self.m is None:
+            self.m, self.v = [], []
+            for param in params:
+                self.m.append(np.zeros_like(param))
+                self.v.append(np.zeros_like(param))
 
-in_layer0 = MatMul(W_in)
-in_layer1 = MatMul(W_in)
-out_layer = MatMul(W_out)
+        self.iter += 1
+        lr_t = self.lr * np.sqrt(1.0 - self.beta2 **
+                                 self.iter) / (1.0 - self.beta1**self.iter)
 
-h0 = in_layer0.forward(c0)
-h1 = in_layer1.forward(c1)
-h = 0.5*(h0+h1)
-s=out_layer.forward(h)
+        for i in range(len(params)):
+            self.m[i] += (1 - self.beta1) * (grads[i] - self.m[i])
+            self.v[i] += (1 - self.beta2) * (grads[i]**2 - self.v[i])
 
-text = 'You say goodbye and I say hello.'
-corpus,word_to_id,id_to_word=preprocess(text)
+            params[i] -= lr_t * self.m[i] / (np.sqrt(self.v[i]) + 1e-7)
 
-contexts, target = create_contexts_target(corpus,window_size=1)
-vocab_size = len(word_to_id)
-target=convert_one_hot(target,vocab_size)
-contexts=convert_one_hot(contexts,vocab_size)
+
+class Trainer:
+    def __init__(self, model, optimizer):
+        self.model = model
+        self.optimizer = optimizer
+        self.loss_list = []
+        self.eval_interval = None
+        self.current_epoch = 0
+
+    def fit(self, x, t, max_epoch=10, batch_size=32, max_grad=None, eval_interval=20):
+        data_size = len(x)
+        max_iters = data_size // batch_size
+        self.eval_interval = eval_interval
+        model, optimizer = self.model, self.optimizer
+        total_loss = 0
+        loss_count = 0
+
+        start_time = time.time()
+        for epoch in range(max_epoch):
+            # シャッフル
+            idx = np.random.permutation(np.arange(data_size))
+            x = x[idx]
+            t = t[idx]
+
+            for iters in range(max_iters):
+                batch_x = x[iters*batch_size:(iters+1)*batch_size]
+                batch_t = t[iters*batch_size:(iters+1)*batch_size]
+
+                # 勾配を求め、パラメータを更新
+                loss = model.forward(batch_x, batch_t)
+                model.backward()
+                params, grads = remove_duplicate(
+                    model.params, model.grads)  # 共有された重みを1つに集約
+                if max_grad is not None:
+                    clip_grads(grads, max_grad)
+                optimizer.update(params, grads)
+                total_loss += loss
+                loss_count += 1
+
+                # 評価
+                if (eval_interval is not None) and (iters % eval_interval) == 0:
+                    avg_loss = total_loss / loss_count
+                    elapsed_time = time.time() - start_time
+                    print('| epoch %d |  iter %d / %d | time %d[s] | loss %.2f'
+                          % (self.current_epoch + 1, iters + 1, max_iters, elapsed_time, avg_loss))
+                    self.loss_list.append(float(avg_loss))
+                    total_loss, loss_count = 0, 0
+
+            self.current_epoch += 1
+
+    def plot(self, ylim=None):
+        x = np.arange(len(self.loss_list))
+        if ylim is not None:
+            plt.ylim(*ylim)
+        plt.plot(x, self.loss_list, label='train')
+        plt.xlabel('iterations (x' + str(self.eval_interval) + ')')
+        plt.ylabel('loss')
+        plt.show()
+
+
+def remove_duplicate(params, grads):
+    '''
+    パラメータ配列中の重複する重みをひとつに集約し、
+    その重みに対応する勾配を加算する
+    '''
+    params, grads = params[:], grads[:]  # copy list
+
+    while True:
+        find_flg = False
+        L = len(params)
+
+        for i in range(0, L - 1):
+            for j in range(i + 1, L):
+                # 重みを共有する場合
+                if params[i] is params[j]:
+                    grads[i] += grads[j]  # 勾配の加算
+                    find_flg = True
+                    params.pop(j)
+                    grads.pop(j)
+                # 転置行列として重みを共有する場合（weight tying）
+                elif params[i].ndim == 2 and params[j].ndim == 2 and \
+                        params[i].T.shape == params[j].shape and np.all(params[i].T == params[j]):
+                    grads[i] += grads[j].T
+                    find_flg = True
+                    params.pop(j)
+                    grads.pop(j)
+
+                if find_flg:
+                    break
+            if find_flg:
+                break
+
+        if not find_flg:
+            break
+
+    return params, grads
+
 
 class SimpleCBOW:
-  def __init__(self,vocab_size,hidden_size):
-    V,H=vocab_size,hidden_size
+    def __init__(self, vocab_size, hidden_size):
+        V, H = vocab_size, hidden_size
 
-    # 重みの初期化
-    W_in=0.01*np.random.randn(V,H).astype('f')
-    W_out=0.01*np.random.randn(V,H).astype('f')
+        # 重みの初期化
+        W_in = 0.01*np.random.randn(V, H).astype('f')
+        W_out = 0.01*np.random.randn(H, V).astype('f')
 
-    # レイヤの作成
-    self.in_layer0=MatMul(W_in)
-    self.in_layer1=MatMul(W_in)
-    self.out_layer=MatMul(W_out)
-    self.loss_layer=SoftmaxWithLoss()
+        # レイヤの作成
+        self.in_layer0 = MatMul(W_in)
+        self.in_layer1 = MatMul(W_in)
+        self.out_layer = MatMul(W_out)
+        self.loss_layer = SoftmaxWithLoss()
 
-    # 全ての重みと勾配をリストにまとめる
-    layers=[self.in_layer0,self.in_layer1,self.out_layer]
-    self.params,self.grads=[],[]
-    for layer in layers:
-      self.params+=layer.params
-      self.grads+=layer.grads
+        # 全ての重みと勾配をリストにまとめる
+        layers = [self.in_layer0, self.in_layer1, self.out_layer]
+        self.params, self.grads = [], []
+        for layer in layers:
+            self.params += layer.params
+            self.grads += layer.grads
 
-    self.word_vecs=W_in
-    
-  def forward(self,contexts,target):
-    h0=self.in_layer0.forward(contexts[:,0])
-    h1=self.in_layer0.forward(contexts[:,1])
-    h=(h0+h1)*0.5
-    score=self.out_layer.forward(h)
-    loss=self.loss_layer.forward(score,target)
-    return loss
-  
-  def backward(self,dout=1):
-    ds= self.loss_layer.backward(dout)
-    da=self.out_layer.backward(ds)
-    da*=0.5
-    self.in_layer0.backward(da)
-    self.in_layer1.backward(da)
-    return None
-  
+        self.word_vecs = W_in
+
+    def forward(self, contexts, target):
+        h0 = self.in_layer0.forward(contexts[:, 0])
+        h1 = self.in_layer1.forward(contexts[:, 1])
+        h = (h0 + h1) * 0.5
+        score = self.out_layer.forward(h)
+        loss = self.loss_layer.forward(score, target)
+        return loss
+
+    def backward(self, dout=1):
+        ds = self.loss_layer.backward(dout)
+        da = self.out_layer.backward(ds)
+        da *= 0.5
+        self.in_layer0.backward(da)
+        self.in_layer1.backward(da)
+        return None
+
+
+window_size = 1
+hidden_size = 5
+batch_size = 3
+max_epoch = 1000
+
+text = 'You say goodbye and I say hello.'
+corpus, word_to_id, id_to_word = preprocess(text)
+
+vocab_size = len(word_to_id)
+contexts, target = create_contexts_target(corpus, window_size)
+target = convert_one_hot(target, vocab_size)
+contexts = convert_one_hot(contexts, vocab_size)
+
+model = SimpleCBOW(vocab_size, hidden_size)
+optimizer = Adam()
+trainer = Trainer(model, optimizer)
+
+trainer.fit(contexts, target, max_epoch, batch_size)
+trainer.plot()
+
+word_vecs = model.word_vecs
+for word_id, word in id_to_word.items():
+    print(word, word_vecs[word_id])
